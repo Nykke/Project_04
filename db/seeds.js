0ce641e8c3644ccf9a64ee9e0cc4f14bb47ee2b9
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-04-02 16:11:59 -0400

    seeds are loaded

--- db/seeds.js
+++ db/seeds.js
@@ -1,16 +1,69 @@
-const mongoose = require('mongoose');
-const seeder = require("mongoose-seed");
+const Maintenance_Request = require("./models.js").Maintenance_Request;
+const User = require("./models.js").User;
+const seedData = require("./seedData.json");
 
-seeder.connect('mongodb://localhost/project_04_db', function(){
+Maintenance_Request.remove({}, err =>{
+  if (err){
+    console.log(err);
+  }
+});
 
-  seeder.loadModels(
-    User = require("./models.js").User,
-    Maintenance_Request = ("./models.js").Maintenance_Request
-  );
+User.remove({}, err => {
+  if (err){
+    console.log(err);
+  }
+});
 
-  seeder.clearModels(["Maintenance_Request", "User"], function(){
-    seeder.populateModels("./seedData.json", function(){
-      seeder.disconnect();
-    })
-  })
-})
+var tom = new User({
+  category: "tenant",
+  division : "apartment"
+});
+
+var dede = new User({
+  category: "landlord",
+  division: "management"
+});
+
+var bathroom = new Maintenance_Request({
+  tenant_name: "Tom",
+  building_number: 17101,
+  apt_number: 101,
+  type: "bathroom",
+  urgency: "high",
+  description: "show is overflowing when water is turned on",
+  completed: "no"
+});
+
+var kitchen = new Maintenance_Request({
+  tenant_name: "Rachel",
+  building_number: 20033,
+  apt_number: 204,
+  type: "kitchen",
+  urgency: "medium",
+  description: "ligt is flickering",
+  completed: "no"
+});
+
+var rachel = new User({
+  category: "tenant",
+  division: "apartment"
+});
+
+var maria = new User({
+  category: "landlord",
+  division: "apartment"
+});
+
+var users = [tom, dede, rachel, maria];
+var maintenance_requests = [bathroom, kitchen];
+
+users.forEach(function(user,i){
+  user.maintenance_requests.push(maintenance_requests[i], maintenance_requests[i+1]); 
+  user.save(function(err){
+    if (err){
+      console.log(err);
+    } else {
+      console.log("request made!");
+    }
+  });
+});
