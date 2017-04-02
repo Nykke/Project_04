08d1385543251422746dc6bb8512b7a206b6036e
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-04-02 15:49:32 -0400

    tried using mongoose-seed

--- db/seeds.js
+++ db/seeds.js
@@ -1,13 +1,16 @@
-const seeder = require("mongoose-seeder");
+const mongoose = require('mongoose');
+const seeder = require("mongoose-seed");
 
-seeder.connect('mongodb://localhost/project_04', function(){
+seeder.connect('mongodb://localhost/project_04_db', function(){
 
   seeder.loadModels(
     User = require("./models.js").User,
-    Maintenance_Request = require("./models.js").Maintenance_Request
+    Maintenance_Request = ("./models.js").Maintenance_Request
   );
 
   seeder.clearModels(["Maintenance_Request", "User"], function(){
-    seeder.populateModels("./seedData.json")
+    seeder.populateModels("./seedData.json", function(){
+      seeder.disconnect();
+    })
   })
 })
