0a25e503da1203636e3fb6a9d480f09ef2b3cf49
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-04-02 13:36:12 -0400

    MVP models created

---
+++ db/models.js
@@ -0,0 +1,25 @@
+const mongoose = require('./connection.js')
+
+const Maintenance_RequestSchema = new mongoose.Schema({
+  tenant_name: String,
+  building_number: Number,
+  apt_number: Number,
+  type: String,
+  urgency: String,
+  description: String,
+  completed: String 
+})
+
+const UserSchema = new mongoose.Schema({
+  category: String,
+  division: String,
+  maintenance_requests: [ {type: Schema.ObjectId, ref:"Maintenance_Request"} ]
+})
+
+const Maintenance_Request = mongoose.model("Maintenance_Request", Maintenance_RequestSchema);
+const User = mongoose.model("User", UserSchema);
+
+module.exports ={
+  Maintenance_Request: Maintenance_Request,
+  User: User
+};
