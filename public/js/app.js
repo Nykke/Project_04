82cf40f4ed42618e31ea4735ac4ac063ac8626a7
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-04-02 19:41:42 -0400

    express api linked, routes configured, welcome route view defined

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,29 @@
-angular.module("project4")
+angular
+  .module("project4", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    Router
+  ])
+  .factory("Maintenance_RequestFactory", [
+    "$resource",
+    Maintenance_RequestFactoryFunction
+  ])
+
+  //defining the main route view
+  function Router($stateProvider) {
+    $stateProvider
+    .state("welcome", {
+      url:"/",
+      templateUrl: "/assets/js/ng-views/welcome.html"
+    })
+  }
+
+  //link to our backend api
+  function Maintenance_RequestFactoryFunction($resource) {
+    return $resource("/api/maintenance_requests/:tenant_name", {}, {
+      update: {method: "PUT"}
+    })
+  }
