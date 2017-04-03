de51315b2f0e016d2a96b99e4a05022930287a89
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-04-03 12:50:17 -0400

    adjusted show and set parser to json

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 //defining express
 const express = require("express");
 const app = express();
+
 //server for angular
 const http = require("http").Server(app);
+
 //defining body-parser
 const parser = require("body-parser");
+
+//pulling schemas from models
 const User = require("./db/models.js").User;
 const Maintenance_Request = require("./db/models.js").Maintenance_Request;
+
+//connect to mongoose
+const mongoose = require("./db/connection.js");
 
 app.set("port", process.env.PORT || 3001)
 
 //the public folder is now defined as assets
 app.use("/assets", express.static("public"))
+
 //configuring body-parser to support html forms
-app.use(parser.urlencoded({extended: true}))
+app.use(parser.json({extended: true}))
 
 //connecting to angular
 app.get("/", function(req, res) {
@@ -27,9 +35,16 @@ app.get("/api/maintenance_requests", function(req, res){
   });
 })
 
+//route defined for the showing maintenance_requests
+app.get("/api/maintenance_requests/:tenant_name", function(req, res){
+  Maintenance_Request.findOne({tenant_name: req.params.tenant_name}).then(function(maintenance_request){
+    res.json(maintenance_request)
+  })
+})
+
 //route defined to create a maintenance_request
 app.post("/api/maintenance_requests", function(req, res){
-  Maintenance_Request.create(req.body.maintenance_request).then(function(maintenance_request){
+  Maintenance_Request.create(req.body).then(function(maintenance_request){
     res.json(maintenance_request);
   });
 })
@@ -46,13 +61,6 @@ app.put("/api/maintenance_requests/:tenant_name", function(req, res){
 app.delete("/api/maintenance_request/:tenant_name", function(req, res){
   Maintenance_Request.findOneandRemove({tenant_name: req.params.tenant_name}).then(function(){
     res.json({success: true});
-  });
-})
-
-//route defined for the showing maintenance_requests
-app.get("/api/maintenance_requests/:tenant_name", function(req, res){
-  Maintenance_Request.findOne({tenant_name: req.params.tenant_name}).then(function(maintenance_request){
-    res.json(maintenance_request);
   });
 })
 
