8669e0cb568e2c2148af150159d8ac1f400d2f22
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-04-02 20:22:31 -0400

    index controller, view and function defined

--- public/js/app.js
+++ public/js/app.js
@@ -11,13 +11,23 @@ angular
     "$resource",
     Maintenance_RequestFactoryFunction
   ])
+  .controller("Maintenance_RequestIndexController", [
+    "Maintenance_RequestFactory",
+    Maintenance_RequestIndexControllerFunction
+  ])
 
-  //defining the main route view
-  function Router($stateProvider) {
+
+  function Router($stateProvider) { //defining the main route view
     $stateProvider
     .state("welcome", {
       url:"/",
       templateUrl: "/assets/js/ng-views/welcome.html"
+    })
+    .state("maintenance_index", { //defining the index route view
+      url: "/maintenance_requests",
+      templateUrl: "/assets/js/ng-views/maintenance_index.html",
+      controller: "Maintenance_RequestIndexController",
+      controllerAs: "vm"
     })
   }
 
@@ -26,4 +36,9 @@ angular
     return $resource("/api/maintenance_requests/:tenant_name", {}, {
       update: {method: "PUT"}
     })
+  }
+
+  //setting up what the index controller returns 
+  function Maintenance_RequestIndexControllerFunction( Maintenance_RequestFactory ){
+    this.maintenance_requests = Maintenance_RequestFactory.query();
   }
