61cb787c36b388c2a97eea3006243d9f98cb456f
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-04-07 11:17:57 -0400

    connection changed

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/project_04_db', (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('MongoDB Connected')
-  }
-})
+// mongoose.connect('mongodb://localhost/project_04_db', (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('MongoDB Connected')
+//   }
+// })
+
+if(process.env.NODE_ENV == "production"){
+  mongoose.connect(process.env.MONGODB_URL);
+} else {
+  mongoose.connect("mongodb://localhost/project4");
+}
 
 module.exports = mongoose;
